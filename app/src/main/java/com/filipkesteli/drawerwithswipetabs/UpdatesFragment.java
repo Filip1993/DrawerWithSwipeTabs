10a83bea47f3b01e1083ed80ebd934c003c6ab28
Filip <filip.kesteli@gmail.com>
2016-05-07 15:26:01 +0200

    Stavljeni i AfternoonAskFragmet i EveningAskFragment...

--- app/src/main/java/com/filipkesteli/drawerwithswipetabs/UpdatesFragment.java
+++ app/src/main/java/com/filipkesteli/drawerwithswipetabs/UpdatesFragment.java
@@ -6,6 +6,19 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.widget.AppCompatSeekBar;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.SwitchCompat;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CompoundButton;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 
 /**
@@ -18,12 +31,50 @@ public class UpdatesFragment extends Fragment {
         // Required empty public constructor
     }
 
+    private CardView cvMorningFeel;
+    private CardView cvMorningMotivated;
+    private CardView cvMorningPlans;
+    private CardView cvMorningHappy;
+    private CardView cvMorningAdventure;
+
+    private SwitchCompat swMorningFeel;
+    private SwitchCompat swMorningMotivated;
+    private SwitchCompat swMorningPlans;
+    private SwitchCompat swMorningHappy;
+    private SwitchCompat swMorningAdventure;
+
+    private TextView tvMorningFeel;
+    private TextView tvMorningMotivated;
+    private TextView tvMorningPlans;
+    private TextView tvMorningHappy;
+    private TextView tvMorningAdventure;
+
+    private AppCompatSeekBar sbMorningFeel;
+    private AppCompatSeekBar sbMorningMotivated;
+    private AppCompatSeekBar sbMorningPlans;
+    private AppCompatSeekBar sbMorningHappy;
+    private AppCompatSeekBar sbMorningAdventure;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_updates, container, false);
+
+        View v = inflater.inflate(R.layout.fragment_evening_ask, container, false);
+
+        initWidgets(v);
+        setupListeners();
+
+        return v;
+    }
+
+    private void initWidgets(View v) {
+
+    }
+
+    private void setupListeners() {
+
     }
 
 }
